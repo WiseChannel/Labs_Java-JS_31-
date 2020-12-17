ef9bdd700e7a1363ee3b39d5c4bf73ee70390154
Ilya <ilyaphotoshop17@gmail.com>
2020-12-17 18:13:23 +0200

    [add] create Task_10

--- Task_10/index.js
+++ Task_10/index.js
@@ -1,5 +1,24 @@
-// Патерн Flyweight. Розробити систему обліку та звітів процесів розмноження колонії бактерій.
+// Патерн Flyweight.
+// Розробити систему обліку та звітів процесів розмноження колонії бактерій.
 
 class VisibilityAndReportingSystem {
-    
+    constructor(name) {
+        this.name = name
+    }
 }
+
+class BacterialColonies {
+    constructor(name) {
+        this.bacterium = {};
+    }
+    create(name) {
+        let bacterium = this.bacterium[name];
+        if (bacterium) return bacterium;
+        this.bacterium[name] = new VisibilityAndReportingSystem(name);
+        return this.bacterium[name];
+    }
+}
+
+export {
+    BacterialColonies
+};
