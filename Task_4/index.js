4144b3c60bde8138d2a147b78aac70b288743fcc
Ilya <ilyaphotoshop17@gmail.com>
2020-12-17 12:36:24 +0200

    [add] create Task_4

--- Task_4/index.js
+++ Task_4/index.js
@@ -1,6 +1,52 @@
-// Патерн Factory Method. Фігури гри «тетріс». Реалізувати процес випадкового вибору фігури з фіксованого набору фігур.
-// передбачити появу супер-фігур із збільшеним числом клітин, ніж звичайні.
+// Патерн Factory Method. Фігури гри «тетріс».
+// Реалізувати процес випадкового вибору фігури з фіксованого набору фігур.
+// Передбачити появу супер-фігур із збільшеним числом клітин, ніж звичайні.
 
 class GameTetris {
+    constructor() {
+        this.figurines = [
+            {
+                id: 1,
+                figure: 'Square',
+                size: 10
+            },
+            {
+                id: 2,
+                figure: 'Triangle',
+                size: 7
+            },
+            {
+                id: 3,
+                figure: 'Rectangle',
+                size: 12
+            },
+        ]
+        this.randomF = []
+    }
+
+    RandomSelection() {
+        //Random selection
+        const randomIndex = Math.floor(Math.random() * 4);
+        const mus = this.figurines[randomIndex]
+        this.randomF.push(mus)
+        console.log(mus)
+
+        // Init create super figure
+        setTimeout(() => {
+            this.SuperFigure(mus)
+        }, 1000)
+    }
+
+    SuperFigure() {
+        //Find random selection and create supFigure
+        const findFig = this.randomF.map(i => i.size).join()
+        const superF = findFig * 2
+
+        console.log('Create Super Figure:', superF)
+    }
 
 }
+
+const init = new GameTetris()
+init.RandomSelection()
+// init.SuperFigure()
