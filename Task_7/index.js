4ec1c4edc2af6132d6272d059c09b142d8953ff3
Ilya <ilyaphotoshop17@gmail.com>
2020-12-17 17:58:40 +0200

    [add] created Task_7

--- Task_7/index.js
+++ Task_7/index.js
@@ -1,6 +1,56 @@
-// Патерн State. Замовлення на отримання гранту для навчання може перебувати в декількох станах: створено, розглядається,
-// відкладено, відхилено, підтверджено, відкликано і т. д. Визначити логіку зміни станів і розробити конструкцію моделі системи.
+// Патерн State.
+// Замовлення на отримання гранту для навчання може перебувати в декількох станах:
+// створено, розглядається, відкладено, відхилено, підтверджено, відкликано і т. д. Визначити логіку зміни станів і розробити конструкцію моделі системи.
+
+//EducationGrant
+class EducationGrant {
+    constructor(name, nextStatus) {
+        this.name = name;
+        this.nextStatus = nextStatus;
+    }
+
+    next() {
+        return new this.nextStatus();
+    }
+}
+
+class WaitingForCreated extends EducationGrant {
+    constructor() {
+        super('waitingForCreated', Considered);
+    }
+}
+
+class Considered extends EducationGrant {
+    constructor() {
+        super('Considered', Postponed);
+    }
+}
+
+class Postponed extends EducationGrant {
+    constructor() {
+        super('Postponed', Rejected);
+    }
+}
+
+class Rejected extends EducationGrant {
+    constructor() {
+        super('Rejected', Confirmed);
+    }
+}
+
+class Confirmed extends EducationGrant {
+    constructor() {
+        super('Confirmed', Rejected);
+    }
+}
 
 class EducationGrant {
+    constructor() {
+        this.state = new WaitingForCreated();
+    }
 
+    nextState() {
+        this.state = this.state.next();
+    };
 }
+
