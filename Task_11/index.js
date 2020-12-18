435a4779effa69d6e9b552a6d2dde196ac0608e7
Ilya <ilyaphotoshop17@gmail.com>
2020-12-18 17:49:58 +0200

    [add] create Task_11

--- Task_11/index.js
+++ Task_11/index.js
@@ -1,7 +1,28 @@
-// Існує модель системи Рецепт. Модель дозволяє в незмінному вигляді зберігати призначення лікаря і термін дії рецепта.
-// Написати код додатка, що дозволяє продовжувати термін дії вже існуючого рецепта. Виберіть відповідний патерн
-// для реалізації цього завдання
+// Існує модель системи Рецепт.
+// Модель дозволяє в незмінному вигляді зберігати призначення лікаря і термін дії рецепта.
+// Написати код додатка, що дозволяє продовжувати термін дії вже існуючого рецепта.
+// Виберіть відповідний патерн для реалізації цього завдання
 
 class DrugAccountingSystem {
+    constructor() {
+        this.docAppointment = {
+            date: Date.now(),
+            nameDoc: 'Влади́мир Влади́мирович Пу́тин',
+            drugName: 'Novichok',
+            prolongation: false
+        }
+    }
 
+    status() {
+        console.log(this.docAppointment)
+    }
+
+    extendThePeriod() {
+        this.docAppointment.date = Date.now()
+        this.docAppointment.prolongation = true
+        console.log(this.docAppointment)
+    }
 }
+
+const init = new DrugAccountingSystem()
+init.extendThePeriod()
